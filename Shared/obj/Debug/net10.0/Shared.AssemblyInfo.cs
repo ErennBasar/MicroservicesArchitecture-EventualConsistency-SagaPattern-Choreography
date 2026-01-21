d82d38214c2aecd99d7667122fcd9cd7356af33c
ErennBasar <2200656020@nku.edu.tr>
2026-01-21 18:06:57 +0300

    nlog update

--- Shared/obj/Debug/net10.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net10.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf284534221504cf5e3e64304e2338a5dbad3cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b63e5a83852d91b985d35f4913d4fa89584d84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
